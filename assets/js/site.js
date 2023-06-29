5c8078640bdf578d8a7fd686931f6ab1566f75f7
Jennings Anderson <jenningsanderson@colorado.edu>
2023-06-29 08:20:54 -0700

    adding activity dashboard
    Former-commit-id: e18244bf3effee6013e44251fc700f2a0e0b2bec

---
+++ assets/js/site.js
@@ -0,0 +1,29 @@
+var site = {
+	monthly_activity_all_time: 'data/monthly_activity_all_time.json',
+	monthly_activity_last_year: 'data/monthly_activity_last_year.json',
+	monthly_activity_last_1200_days: 'data/monthly_activity_last_1200_days.json',
+	top_edited_countries: 'data/top_edited_countries.json',
+	// mapbox_tileset_id: 'jenningsanderson.ym_changesets',
+	mapbox_tileset_id: 'jenningsanderson.442aud42',
+	// mapbox_tileset_id: 'jenningsanderson.0nzzv7oh',
+	// LOCAL_PORT: 3001
+}
+
+var prettyFormat = function(bigInt){
+
+	if (bigInt > 1000000){
+		return (bigInt / 1000000).toFixed(2)+'M'
+	}
+
+	if (bigInt > 1000){
+		return (bigInt / 1000).toFixed(2)+'K'
+	}
+
+}
+
+function numberWithCommas(x) {
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+const MONTHS = ['January','February','March','April','May','June',
+                'July','August','September','October','November','December']
